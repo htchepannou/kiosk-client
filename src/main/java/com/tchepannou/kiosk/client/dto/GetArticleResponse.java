1750f2567c528d25c34338ed0d8c607f8e757f4f
htchepannou <htchepannou@expedia.com>
2016-09-17 07:35:59 -0400

    fix

--- src/main/java/com/tchepannou/kiosk/client/dto/GetArticleResponse.java
+++ src/main/java/com/tchepannou/kiosk/client/dto/GetArticleResponse.java
@@ -1,20 +1,13 @@
 package com.tchepannou.kiosk.client.dto;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GetArticleResponse extends AbstractResponse {
-    private List<ArticleDto> articles = new ArrayList<>();
+    private ArticleDto article;
 
-    public List<ArticleDto> getArticles() {
-        return articles;
+    public ArticleDto getArticle() {
+        return article;
     }
 
-    public void setArticles(final List<ArticleDto> articles) {
-        this.articles = articles;
-    }
-
-    public int getSize() {
-        return articles.size();
+    public void setArticle(final ArticleDto article) {
+        this.article = article;
     }
 }

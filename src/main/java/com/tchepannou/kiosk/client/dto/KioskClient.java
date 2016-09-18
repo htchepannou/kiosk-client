5272a66ca2718ef489efb50bc7af662c502a433f
htchepannou <htchepannou@expedia.com>
2016-09-18 06:51:26 -0400

    return list of articles

--- src/main/java/com/tchepannou/kiosk/client/dto/KioskClient.java
+++ src/main/java/com/tchepannou/kiosk/client/dto/KioskClient.java
@@ -3,5 +3,9 @@ package com.tchepannou.kiosk.client.dto;
 public interface KioskClient {
     FeedListResponse getFeeds();
 
+    GetArticleResponse getArticle (final String articleId);
+
+    GetArticleListResponse getArticlesByStatus (final String status);
+
     PublishResponse publishArticle(PublishRequest request);
 }

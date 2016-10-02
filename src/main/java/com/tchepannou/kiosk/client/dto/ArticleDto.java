3c57e615c172be45c338c7ab905c0f9af28b7aad
htchepannou <htchepannou@expedia.com>
2016-10-02 10:26:54 -0400

    add website and image to ArticleDTO

--- src/main/java/com/tchepannou/kiosk/client/dto/ArticleDto.java
+++ src/main/java/com/tchepannou/kiosk/client/dto/ArticleDto.java
@@ -2,6 +2,8 @@ package com.tchepannou.kiosk.client.dto;
 
 public class ArticleDto extends ArticleDataDto {
     private String id;
+    private WebsiteDto website;
+    private ImageDto image;
 
     public String getId() {
         return id;
@@ -9,5 +11,21 @@ public class ArticleDto extends ArticleDataDto {
 
     public void setId(final String id) {
         this.id = id;
+    }
+
+    public WebsiteDto getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(final WebsiteDto website) {
+        this.website = website;
+    }
+
+    public ImageDto getImage() {
+        return image;
+    }
+
+    public void setImage(final ImageDto image) {
+        this.image = image;
     }
 }
--- src/main/java/com/tchepannou/kiosk/client/dto/GetArticleResponse.java
+++ src/main/java/com/tchepannou/kiosk/client/dto/GetArticleResponse.java
@@ -2,8 +2,6 @@ package com.tchepannou.kiosk.client.dto;
 
 public class GetArticleResponse extends AbstractResponse {
     private ArticleDto article;
-    private WebsiteDto website;
-    private ImageDto image;
 
     public ArticleDto getArticle() {
         return article;
@@ -11,21 +9,5 @@ public class GetArticleResponse extends AbstractResponse {
 
     public void setArticle(final ArticleDto article) {
         this.article = article;
-    }
-
-    public WebsiteDto getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(final WebsiteDto website) {
-        this.website = website;
-    }
-
-    public ImageDto getImage() {
-        return image;
-    }
-
-    public void setImage(final ImageDto image) {
-        this.image = image;
     }
 }

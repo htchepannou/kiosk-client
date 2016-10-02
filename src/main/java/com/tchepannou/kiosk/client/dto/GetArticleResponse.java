0c0621051973bfb66cf129c37af91ff99c693cab
htchepannou <htchepannou@expedia.com>
2016-10-02 00:02:35 -0400

    add image DTO

--- src/main/java/com/tchepannou/kiosk/client/dto/GetArticleResponse.java
+++ src/main/java/com/tchepannou/kiosk/client/dto/GetArticleResponse.java
@@ -3,6 +3,7 @@ package com.tchepannou.kiosk.client.dto;
 public class GetArticleResponse extends AbstractResponse {
     private ArticleDto article;
     private WebsiteDto website;
+    private ImageDto image;
 
     public ArticleDto getArticle() {
         return article;
@@ -18,5 +19,13 @@ public class GetArticleResponse extends AbstractResponse {
 
     public void setWebsite(final WebsiteDto website) {
         this.website = website;
+    }
+
+    public ImageDto getImage() {
+        return image;
+    }
+
+    public void setImage(final ImageDto image) {
+        this.image = image;
     }
 }
